23b8d62ceee317809304076ce03e4e2836e0fc9a
Togira <70365614+Togira123@users.noreply.github.com>
2026-03-10 22:07:27 +0100

    added unregister to app handler

--- goblend/__init__.py
+++ goblend/__init__.py
@@ -7,9 +7,9 @@ from .src.config import get_config_at_startup
 
 def register():
     export_ui.register()
-
     bpy.app.handlers.load_post.append(get_config_at_startup)
 
 
 def unregister():
     export_ui.unregister()
+    bpy.app.handlers.load_post.remove(get_config_at_startup)
